552a3a1cef42def7228c523cf394915165b94a36
Carlos Henrique dos Santos <carloshenriquedossantos@RJ-MB4323.local>
2022-03-06 16:34:22 -0300

    projetct skeleton

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+    const query = request.query;
+    console.log(query);
+   return response.json(["curso1", "curso2", "curso3"]);
+
+});
+
+app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json(["curso1", "curso2", "curso3"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+    const { id } = request.params;
+    console.log(id)
+    return response.json(["curso13", "curso2", "curso3"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["curso13", "curso2", "curso3"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json({message: "curso deletad com sucesso"});
+});
+app.listen(3333);
